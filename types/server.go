36222ab81a6974fb4023d36182975e550e2cbff5
maruki00 <aitbaha48@gmail.com>
2025-05-14 11:26:42 +0100

    commit

--- types/server.go
+++ types/server.go
@@ -1,8 +1,10 @@
 package types
 
 import (
+	"net"
 	"net/http/httputil"
 	"sync"
+	"time"
 )
 
 type Server struct {
@@ -12,4 +14,20 @@ type Server struct {
 	LastTimeOutResponse int
 	Wieght              int
 	Proxy               *httputil.ReverseProxy
+}
+
+func (_this *Server) checkServerAlive(timeOut int) bool {
+	ATTEMPTS := 3
+	for {
+		conn, err := net.DialTimeout("tcp", _this.Addr, time.Duration(time.Second*time.Duration(timeOut)))
+		conn.Close()
+		if err == nil {
+			return true
+		}
+		if ATTEMPTS <= 0 {
+			return false
+		}
+		ATTEMPTS--
+	}
+	return true
 }
